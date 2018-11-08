1bcbd87b62cd5a3e83d047637b3433edf81538ac
Mårten Pettersson <mtnptrsn@gmail.com>
2018-11-08 11:54:26 +0100

    RNCWKWebView: JSON.stringifying postmessage data if it's an array or object

--- ios/RNCWKWebView.m
+++ ios/RNCWKWebView.m
@@ -413,7 +413,7 @@ static NSString *const MessageHanderName = @"ReactNative";
         "window.originalPostMessage = window.postMessage;"
 
         "window.postMessage = function(data) {"
-          "window.webkit.messageHandlers.%@.postMessage(String(data));"
+          "window.webkit.messageHandlers.%@.postMessage(typeof data === 'object' ? JSON.stringify(data) : String(data));"
         "};"
       "})();",
       MessageHanderName
